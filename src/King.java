2131280fbba16090e7739ca3b80b2b75ec84f3d0
FreddySteel <17steel_f@bexleygs.co.uk>
2022-12-14 10:13:37 +0000

    Initial commit

---
+++ src/King.java
@@ -0,0 +1,31 @@
+public class King extends Piece {
+    private boolean inCheck;
+
+    public King(String position, boolean isBlack) {
+        super(position, isBlack);
+        symbol = 'k';
+        inCheck = false;
+    }
+
+    // Kings can move in 1 square in any direction
+    public boolean isValidMove(String targetPosition) {
+        // convert position to row and column
+        int startRow = ChessUtils.getRowFromPosition(getPosition());
+        int startColumn = ChessUtils.getColumnFromPosition(getPosition());
+        int targetRow = ChessUtils.getRowFromPosition(targetPosition);
+        int targetColumn = ChessUtils.getColumnFromPosition(targetPosition);
+        // can't stand still
+        if (targetRow == startRow && targetColumn == startColumn) {
+            return false;
+        }
+        // can't move more than 1 square in any direction
+        if (Math.abs(startRow - targetRow) > 1) {
+            return false;
+        }
+        if (Math.abs(startColumn - targetColumn) > 1) {
+            return false;
+        }
+        // otherwise we cool
+        return true;
+    }
+}
---
+++ src/Piece.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Piece {
+    private int row;
+    private int column;
+    private boolean isBlack;
+    protected char symbol;
+
+    public Piece(int row, int column, boolean isBlack) {
+        this.row = row;
+        this.column = column;
+        this.isBlack = isBlack;
+        symbol = 'x';  // dummy symbol - shouldn't ever appear on the board
+    }
+
+    public Piece(String position, boolean isBlack) {
+        this.isBlack = isBlack;
+        symbol = 'x';
+        row = ChessUtils.getRowFromPosition(position);
+        column = ChessUtils.getColumnFromPosition(position);
+    }
+
+
+    public char getSymbol() {
+        if (isBlack) {
+            return Character.toUpperCase(symbol);
+        }
+        return Character.toLowerCase(symbol);
+    }
+
+    // convert row and column values to chess Notation eg 0,0 -> A1
+    public String getPosition() {
+        return ChessUtils.columnToLetter(column) + ChessUtils.rowToNumber(row);
+    }
+
+    // set a new position for the piece
+    public void setPosition(String newPosition) {
+        // add code here
+    }
+
+    public boolean isValidMove(String targetPosition) {
+        // can't stand still
+        if (getPosition().equals(targetPosition)) {
+            return false;
+        }
+        return true;
+    }
+
+    // return an empty list by default
+    public ArrayList<String> passesThrough(String position) {
+        return new ArrayList<>();
+    }
+}
